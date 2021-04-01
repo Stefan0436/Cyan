23332f2a2e2c6599301d40ae8515709e072996ec
Stefan Eekhof <stefaneekhof@aerialworks.ddns.net>
2021-04-01 10:24:49 +0200

    Fixed windows script

--- CCFG/cq-script/cq.bat
+++ CCFG/cq-script/cq.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 
 SET libs=
-for /r "C:\Program Files (x86)\CCFG-CQ-LIBS" %%i in (*.jar) do SET libs=!libs!:%%i
+for /r "C:\Program Files (x86)\CCFG-CQ-LIBS" %%i in (*.jar) do SET libs=!libs!;%%i
 SET libs=%libs:~1%
 
 java -cp "%libs%" org.asf.cyan.api.config.CqMain %*
