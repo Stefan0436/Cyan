5aa0daf4b92a963c6cfc28b45282d9fe1258b99e
Stefan Eekhof <stefaneekhof@aerialworks.ddns.net>
2021-04-13 16:47:28 +0200

    Update cornflower main extension; added http url scheme for the cyan trust connective server module

--- Cornflower/src/main/java/org/asf/cyan/cornflower/gradle/flowerinternal/projectextensions/CornflowerMainExtension.java
+++ Cornflower/src/main/java/org/asf/cyan/cornflower/gradle/flowerinternal/projectextensions/CornflowerMainExtension.java
@@ -6,7 +6,21 @@ public class CornflowerMainExtension implements IProjectExtension {
 	public static final Class<org.asf.cyan.cornflower.classpath.util.SourceType> SourceType = org.asf.cyan.cornflower.classpath.util.SourceType.class;
 	public static final Class<org.asf.cyan.cornflower.classpath.util.EntryType> EntryType = org.asf.cyan.cornflower.classpath.util.EntryType.class;
 	public static final Class<org.asf.cyan.cornflower.classpath.util.PathPriority> PathPriority = org.asf.cyan.cornflower.classpath.util.PathPriority.class;
-	
+
 	public static final Class<?> EclipseLaunchGenerator = org.asf.cyan.cornflower.gradle.tasks.EclipseLaunchGenerator.class;
 	public static final Class<?> CtcUtil = org.asf.cyan.cornflower.gradle.tasks.CtcTask.class;
+
+	public static String connectiveHttpURLScheme(String server, String group, String modid, String modversion,
+			String trustname) {
+		String url = "/cyan/trust/upload/" + group + "/" + modid + "?trustname=" + trustname + "&modversion="
+				+ modversion + "&file=";
+
+		while (url.contains("//"))
+			url = url.replace("//", "/");
+		while (server.endsWith("/")) {
+			server = server.substring(0, server.lastIndexOf("/"));
+		}
+
+		return server + url;
+	}
 }
