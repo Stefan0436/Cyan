58fce2c0bc08723004ac97044f768fd76f271675
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-11 16:56:27 +0200

    Patch workflow build script

--- buildworkflow.sh
+++ buildworkflow.sh
@@ -2,13 +2,13 @@
 source ./workflowbuilddata-$1.bash
 cmd="--version \"$1\""
 if [ "$2" != "" ]; then
-    cmd+="--modloader \"$2\""
+    cmd+=" --modloader \"$2\""
     if [ "$2" == "forge" ]; then
-        cmd+="--modloader-version \"$forge\""
+        cmd+=" --modloader-version \"$forge\""
     elif [ "$2" == "fabric" ]; then
-        cmd+="--modloader-version \"$fabric\" --mappings-version \"$mappings_fabric\""
+        cmd+=" --modloader-version \"$fabric\" --mappings-version \"$mappings_fabric\""
     elif [ "$2" == "paper" ]; then
-        cmd+="--modloader-version \"$paper\" --mappings-version \"$mappings_paper\""
+        cmd+=" --modloader-version \"$paper\" --mappings-version \"$mappings_paper\""
     fi
 fi
 
