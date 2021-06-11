8dfa32deb623aac605cea97922204323d19dd154
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-11 11:49:26 +0200

    Update building files

--- build.release.bash
+++ build.release.bash
@@ -13,11 +13,16 @@ function prepare() {
 }
 
 function build() {
+    gameVersions=( "1.16.5" "1.17" )
+    
     chmod +x gradlew buildlocal.sh
     ./gradlew -c settings.lite.gradle installLiteLibs
-    ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true processResources
-    ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true processResources
-    ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true serverDownloads
+    
+    for ver in "${gameVersions[@]}"; do
+        ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" processResources
+        ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" processResources
+        ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" serverDownloads
+    done
 }
 
 function install() {
