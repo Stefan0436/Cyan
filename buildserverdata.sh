ff64b6f05361f8f72cc2f16a4fc3329cdd89e930
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-17 15:47:23 +0200

    Update build script for all server data

--- buildserverdata.sh
+++ buildserverdata.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 gameVersions=( "1.16.5" "1.17" )
 
+echo Building LiteCyan...
 chmod +x gradlew buildlocal.sh
 ./gradlew -c settings.lite.gradle installLiteLibs
 
 for ver in "${gameVersions[@]}"; do
+    echo Building for "$ver"...
+    echo Processing resources...
+    
     ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" processResources
     ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" processResources
+    
+    echo Building server data...
     ./gradlew -PcurrentXML -PnoServer=true -PnoClient=true -PuncheckedClient=true -PaddChangeLog="$(cat change.log)" -PoverrideGameVersion="$ver" serverDownloads
+    
+    echo
 done
