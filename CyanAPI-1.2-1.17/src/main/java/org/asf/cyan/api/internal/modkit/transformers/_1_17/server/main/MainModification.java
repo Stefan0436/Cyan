1bf2f0f1b0ebb995e4bc46fbc0bc518d2a4cc7fa
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-19 22:49:59 +0200

    Re-added the spigot main modifications

--- CyanAPI-1.2-1.17/src/main/java/org/asf/cyan/api/internal/modkit/transformers/_1_17/server/main/MainModification.java
+++ CyanAPI-1.2-1.17/src/main/java/org/asf/cyan/api/internal/modkit/transformers/_1_17/server/main/MainModification.java
@@ -1,12 +1,15 @@
 package org.asf.cyan.api.internal.modkit.transformers._1_17.server.main;
 
+import org.asf.cyan.api.fluid.annotations.PlatformExclude;
 import org.asf.cyan.api.internal.ModKitController;
+import org.asf.cyan.api.modloader.information.game.LaunchPlatform;
 import org.asf.cyan.fluid.api.FluidTransformer;
 import org.asf.cyan.fluid.api.transforming.InjectAt;
 import org.asf.cyan.fluid.api.transforming.TargetClass;
 import org.asf.cyan.fluid.api.transforming.enums.InjectLocation;
 
 @FluidTransformer
+@PlatformExclude(LaunchPlatform.SPIGOT)
 @TargetClass(target = "net.minecraft.server.Main")
 public class MainModification {
 	
