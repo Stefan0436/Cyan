414ad80331dfdccc5fe8a449644ec26306d04f55
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-21 18:11:37 +0200

    Update paper version (EXPERIMENTAL)

--- workflowbuilddata-1.17.bash
+++ workflowbuilddata-1.17.bash
@@ -1,6 +1,6 @@
 forge=
 fabric=0.11.6
-paper=17
+paper=28
 
-mappings_paper=3cec511b16ffa31cb414997a14be313716882e12:1_17_R1
+mappings_paper=33a04d97ab566ac6c8e8a6794e2d432234f3d2e5:PB_28
 mappings_fabric=1.17+build.13
