1da2d1c64312659e4249796f85d6b9fffe195d3b
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-06-22 16:22:58 +0200

    Patch MixinSupport class tracker (MixinSupport is till inactive, just needed to fix a compiler error)

--- MixinSupport/src/main/java/org/asf/cyan/mixin/providers/CyanMixinClassTracker.java
+++ MixinSupport/src/main/java/org/asf/cyan/mixin/providers/CyanMixinClassTracker.java
@@ -3,11 +3,21 @@ package org.asf.cyan.mixin.providers;
 import org.asf.cyan.api.classloading.CyanClassTracker;
 import org.spongepowered.asm.service.IClassTracker;
 
-public class CyanMixinClassTracker extends CyanClassTracker implements IClassTracker {
+public class CyanMixinClassTracker implements IClassTracker {
 
 	@Override
 	public String getClassRestrictions(String className) {
 		return "";
+	}
+
+	@Override
+	public void registerInvalidClass(String className) {
+		CyanClassTracker.registerInvalidClass(className);
+	}
+
+	@Override
+	public boolean isClassLoaded(String className) {
+		return CyanClassTracker.isClassLoaded(className);
 	}
 
 }
