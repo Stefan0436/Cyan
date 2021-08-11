f4195328bbc3cb412bf29301dbcea50d47f91b02
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-08-11 10:35:16 +0200

    Update modloader support: + Forge 1.17.1: 37.0.29 -> 37.0.32 + Paper 1.17.1: 170 -> 172

--- Installer/src/main/java/org/asf/cyan/Installer.java
+++ Installer/src/main/java/org/asf/cyan/Installer.java
@@ -91,7 +91,7 @@ import javax.swing.JCheckBox;
 
 public class Installer extends CyanComponent {
 
-	private static final String version = "4.9";
+	private static final String version = "5.0";
 
 	private static Installer impl;
 
--- workflowbuilddata-1.17.1.bash
+++ workflowbuilddata-1.17.1.bash
@@ -1,5 +1,5 @@
-forge=37.0.29
+forge=37.0.32
 fabric=0.11.6
-paper=170
+paper=172
 
-mappings_paper=80650e893678e726b0d5724031344eb2ec47a51a:PB_170
+mappings_paper=fdef125b195f48e76fa61dfdf00fd7530e424877:PB_172
