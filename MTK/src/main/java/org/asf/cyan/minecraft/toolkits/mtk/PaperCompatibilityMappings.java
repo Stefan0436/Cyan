cb269a321b91f4f32dbc773b7360cc57e1cee317
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2021-08-12 20:14:05 +0200

    Fix inconsistency mappings

--- MTK/src/main/java/org/asf/cyan/minecraft/toolkits/mtk/PaperCompatibilityMappings.java
+++ MTK/src/main/java/org/asf/cyan/minecraft/toolkits/mtk/PaperCompatibilityMappings.java
@@ -129,7 +129,7 @@ public class PaperCompatibilityMappings extends CompatibilityMappings {
 		String mappingsId = "-" + mappingsVersion.replaceAll("[!?/:\\\\]", "-")
 				+ (modloader.isEmpty() ? "" : "-" + modloader);
 
-		setBuild(5);
+		setBuild(6);
 
 		try {
 			if (loadWhenPossible("paper", mappingsId, info, GameSide.SERVER))
