0e9df5d1be61dba6a612bcb6b3bb8954ed7ae72b
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2022-05-16 13:03:48 +0200

    Attempt to fix MTK MSA authentication on windows

--- MTK/src/main/java/org/asf/cyan/minecraft/toolkits/mtk/auth/windowed/MsaAuthWindow.java
+++ MTK/src/main/java/org/asf/cyan/minecraft/toolkits/mtk/auth/windowed/MsaAuthWindow.java
@@ -119,8 +119,7 @@ public class MsaAuthWindow {
 				if (!output.exists())
 					missingLibs.put(baseURL.replace("%proj%", lib).replace("%ver%", jfxVersion).replace("%platform%",
 							OsInfo.getCurrent() == OsInfo.osx ? "mac"
-									: OsInfo.getCurrent() == OsInfo.windows ? "win"
-											: OsInfo.getCurrent().toString()),
+									: OsInfo.getCurrent() == OsInfo.windows ? "win" : OsInfo.getCurrent().toString()),
 							output);
 			}
 
@@ -148,8 +147,8 @@ public class MsaAuthWindow {
 						@SuppressWarnings("resource")
 						DynamicClassLoader loader = new DynamicClassLoader();
 						loader.addLoadRestriction(str -> {
-							return str.contains("org.asf") && !str.contains("SwingFXWebView")
-									&& !str.contains("AuthAdapter") || str.contains("javax.swing");
+							return (str.contains("org.asf") && !str.contains("AuthAdapter")
+									&& !str.contains("SwingFXWebView"));
 						});
 						loader.setOptions(DynamicClassLoader.OPTION_ALLOW_DEFINE);
 						loader.addUrls(urls);
@@ -221,8 +220,8 @@ public class MsaAuthWindow {
 								@SuppressWarnings("resource")
 								DynamicClassLoader loader = new DynamicClassLoader();
 								loader.addLoadRestriction(str -> {
-									return (str.contains("org.asf") && !str.contains("AuthAdapter"))
-											|| str.contains("SwingFXWebView") || str.contains("javax.swing");
+									return (str.contains("org.asf") && !str.contains("AuthAdapter")
+											&& !str.contains("SwingFXWebView"));
 								});
 								loader.setOptions(DynamicClassLoader.OPTION_ALLOW_DEFINE);
 								loader.addUrls(urls);
